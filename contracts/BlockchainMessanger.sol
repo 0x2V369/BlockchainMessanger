7aa12635bc9b16f212f2e94eaf88de1ba9ce765c
0x2V369 <0x2V369@gmail.com>
2024-07-09 17:28:21 +0300

    added comments to main contract

--- contracts/BlockchainMessanger.sol
+++ contracts/BlockchainMessanger.sol
@@ -4,10 +4,6 @@ pragma solidity 0.8.15;
 
 contract BlockchainMessanger{
 
-    // can store a string on blockchain - readable for everyone
-    // message can only be changed by the person who wrote the contract
-    // counter for how many times the message was updated
-
     address owner;
     uint256 public counter;
     string public lastMessage;
@@ -15,6 +11,8 @@ contract BlockchainMessanger{
     constructor(){
         owner = msg.sender;
     }
+
+    /// 
 
     function updateLastMessage(string memory _newMessage) public {
         require(keccak256(abi.encodePacked(_newMessage)) != keccak256(abi.encodePacked(lastMessage)));
